b566b7d2570749a153f30ddd8c0908e784e0d1a6
Sergey <sergeyp3d@rambler.ru>
2024-02-20 14:17:52 +0300

    actix_2fa_db - only postgres and repo are  available now

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,6 @@
 use std::env;
 use std::error::Error;
+use std::sync::Arc;
 use actix_cors::Cors;
 use actix_web::{App, HttpServer, web};
 use actix_web::http::header;
@@ -7,6 +8,9 @@ use actix_web::middleware::Logger;
 use actix_web::web::Data;
 use dotenv::dotenv;
 use env_logger::Builder;
+use crate::db::mongo::MongoRepo;
+use crate::db::Repo;
+use crate::db::sql::postgres::PostgresRepo;
 
 
 mod otp_handlers;
@@ -17,13 +21,13 @@ mod models;
 mod db;
 mod schema;
 
-#[cfg(all(mongo))]
+//#[cfg(all(mongo))]
 pub async fn start_2FA_server_with_mongo_repo() -> std::io::Result<()>
 {
     let (SERVER_HOST, SERVER_PORT, CORS_URL) = prepare_env();
 
-    let app_data = init_mongo_repo().await;
-    let app_data = Data::new(Arc::new(app_data));
+    let repo = MongoRepo::init().await.expect("could not initialize mongo db repository");
+    let app_data = Data::new(Arc::new(repo));
     start_server(CORS_URL.clone(), SERVER_HOST.clone(), SERVER_PORT, app_data).await?;
 
     println!("🚀 Server started successfully");
@@ -31,41 +35,13 @@ pub async fn start_2FA_server_with_mongo_repo() -> std::io::Result<()>
     Ok(())
 }
 
-#[cfg(all(postgres))]
+//#[cfg(all(postgres))]
 pub async fn start_2FA_server_with_postgres_repo() -> std::io::Result<()>
 {
     let (SERVER_HOST, SERVER_PORT, CORS_URL) = prepare_env();
 
-    let app_data = init_postgres_repo().await;
-    let app_data = Data::new(Arc::new(app_data));
-    start_server(CORS_URL.clone(), SERVER_HOST.clone(), SERVER_PORT, app_data).await?;
-
-    println!("🚀 Server started successfully");
-
-    Ok(())
-}
-
-#[cfg(all(mysql))]
-pub async fn start_2FA_server_with_mysql_repo() -> std::io::Result<()>
-{
-    let (SERVER_HOST, SERVER_PORT, CORS_URL) = prepare_env();
-
-    let app_data = init_mysql_repo().await;
-    let app_data = Data::new(Arc::new(app_data));
-    start_server(CORS_URL.clone(), SERVER_HOST.clone(), SERVER_PORT, app_data).await?;
-
-    println!("🚀 Server started successfully");
-
-    Ok(())
-}
-
-#[cfg(all(sqlite))]
-pub async fn start_2FA_server_with_sqlite_repo() -> std::io::Result<()>
-{
-    let (SERVER_HOST, SERVER_PORT, CORS_URL) = prepare_env();
-
-    let app_data = init_sqlite_repo().await;
-    let app_data = Data::new(Arc::new(app_data));
+    let repo = init_postgres_repo().await;
+    let app_data = Data::new(Arc::new(repo));
     start_server(CORS_URL.clone(), SERVER_HOST.clone(), SERVER_PORT, app_data).await?;
 
     println!("🚀 Server started successfully");
@@ -74,48 +50,13 @@ pub async fn start_2FA_server_with_sqlite_repo() -> std::io::Result<()>
 }
 
 
-#[cfg(all(postgres))]
-// async fn init_postgres_repo() -> Result<Box<PostgresRepo<Postgres>>, Box<dyn Error+Send+Sync>>
+//#[cfg(all(postgres))]
 async fn init_postgres_repo() -> Result<Box<PostgresRepo>, Box<dyn Error+Send+Sync>>
 {
-    let DATABASE_URL = env::var("DATABASE_URL").expect("error: no DATABASE_URL env var");
-    let MAX_POOL_CONNECTIONS = env::var("MAX_POOL_CONNECTIONS")
-        .expect("error: no MAX_POOL_CONNECTIONS env var")
-        .parse::<u32>()
-        .expect("parse error");
-    PostgresRepo::init(MAX_POOL_CONNECTIONS, &DATABASE_URL).await
+    PostgresRepo::init().await
 }
 
-#[cfg(all(mysql))]
-// async fn init_mysql_repo() -> Result<Box<MysqlRepo<MySql>>, Box<dyn Error+Send+Sync>>
-async fn init_mysql_repo() -> Result<Box<MysqlRepo>, Box<dyn Error+Send+Sync>>
-{
-    let DATABASE_URL = env::var("DATABASE_URL").expect("error: no DATABASE_URL env var");
-    let MAX_POOL_CONNECTIONS = env::var("MAX_POOL_CONNECTIONS")
-        .expect("error: no MAX_POOL_CONNECTIONS env var")
-        .parse::<u32>()
-        .expect("parse error");
-    MysqlRepo::init(MAX_POOL_CONNECTIONS, &DATABASE_URL).await
-}
 
-#[cfg(all(sqlite))]
-// async fn init_sqlite_repo() -> Result<Box<SqliteRepo<Sqlite>>, Box<dyn Error+Send+Sync>>
-async fn init_sqlite_repo() -> Result<Box<SqliteRepo>, Box<dyn Error+Send+Sync>>
-{
-    let DATABASE_URL = env::var("DATABASE_URL").expect("error: no DATABASE_URL env var");
-    let MAX_POOL_CONNECTIONS = env::var("MAX_POOL_CONNECTIONS")
-        .expect("error: no MAX_POOL_CONNECTIONS env var")
-        .parse::<u32>()
-        .expect("parse error");
-    SqliteRepo::init(MAX_POOL_CONNECTIONS, &DATABASE_URL).await
-}
-
-#[cfg(all(mongo))]
-async fn init_mongo_repo() -> Result<Box<MongoRepo>, Box<dyn Error + Send + Sync>>
-{
-    let MONGO_URI = env::var("MONGO_URI").expect("error: no MONGO_URI env var");
-    MongoRepo::init(0, &MONGO_URI).await
-}
 
 /// returns (SERVER_HOST, SERVER_PORT, CORS_URL) env vars
 fn prepare_env() -> (String, u16, String) {
@@ -125,7 +66,7 @@ fn prepare_env() -> (String, u16, String) {
     if env::var_os("RUST_LOG").is_none() {
         env::set_var("RUST_LOG", "actix_web=info");
     }
-    env_logger::init();
+    //env_logger::init();
 
     let SERVER_HOST = env::var("SERVER_HOST").expect("error: no SERVER_HOST env var");
     let SERVER_PORT = env::var("SERVER_PORT")
@@ -176,53 +117,18 @@ async fn start_server<Repo: Send + Sync + 'static>(
 
 #[cfg(test)]
 mod tests {
-    use actix_cors::Cors;
-    use actix_web::{App, HttpServer, web};
-    use actix_web::http::header;
-    use actix_web::middleware::Logger;
-    use dotenv::dotenv;
-    //#[cfg(all(mongo))]
-    use crate::db::mongo::{MONGO_URI, MongoRepo};
-    use crate::service;
 
-    // #[tokio::test]
-    // #[cfg(all(mongo))]
-    // async fn start_server_with_mongo_repo() -> std::io::Result<()>
-    // {
-    //     dotenv().ok();
-    //
-    //     if std::env::var_os("RUST_LOG").is_none() {
-    //         std::env::set_var("RUST_LOG", "actix_web=info");
-    //     }
-    //     env_logger::init();
-    //
-    //     let mongo_repo = MongoRepo::init(MONGO_URI).await;
-    //     let app_data = web::Data::new(mongo_repo);
-    //
-    //     println!("🚀 Server started successfully");
-    //
-    //     HttpServer::new( move || {
-    //         let cors = Cors::default()
-    //             .allowed_origin("http://localhost:3000")
-    //             .allowed_origin("http://localhost:3000/")
-    //             .allowed_methods(vec!["GET", "POST"])
-    //             .allowed_headers(vec![
-    //                 header::CONTENT_TYPE,
-    //                 header::AUTHORIZATION,
-    //                 header::ACCEPT,
-    //             ])
-    //             .supports_credentials();
-    //
-    //         App::new()
-    //             .app_data(app_data.clone())
-    //             .configure(service::config)
-    //             .wrap(cors)
-    //             .wrap(Logger::default())
-    //     })
-    //         .bind(("127.0.0.1", 8000))?
-    //         .run()
-    //         .await?;
-    //
-    //     Ok(())
-    // }
+    use crate::{service, start_2FA_server_with_mongo_repo, start_2FA_server_with_postgres_repo};
+
+    #[tokio::test]
+    async fn start_server_with_mongo_repo() -> std::io::Result<()>
+    {
+        start_2FA_server_with_mongo_repo().await
+    }
+
+    #[tokio::test]
+    async fn start_server_with_postgres_repo() -> std::io::Result<()>
+    {
+        start_2FA_server_with_postgres_repo().await
+    }
 }
