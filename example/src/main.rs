93c597dd6140fb9d47dae09fbae67d468a092a80
RAprogramm <andrey.rozanov.vl@gmail.com>
2024-02-21 11:41:45 +0900

    xz

--- example/src/main.rs
+++ example/src/main.rs
@@ -1,5 +1,10 @@
+use std::error::Error;
+
 use auth_2fa::start_2FA_server_with_mongo_repo;
 
-fn main() {
-    start_2FA_server_with_mongo_repo()
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn Error>> {
+    let _ = start_2FA_server_with_mongo_repo().await;
+
+    Ok(())
 }
